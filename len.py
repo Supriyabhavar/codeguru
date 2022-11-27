9a990d080b7b3d6eed2dd94e98759271915a272c
sushant.more <sushant.more@peritossolutions.com>
2022-11-27 23:37:55 +0530

    added files

---
+++ len.py
@@ -0,0 +1,6 @@
+course = 'my name is supriya bhavar'
+
+print(course.upper()) 
+print(course.lower())
+print(course.find('n'))
+print(course.replace('supriya','suuu'))
---
+++ var.py
@@ -0,0 +1,5 @@
+first = 'supriya'
+last = 'bhavar'
+message = first + '[' + last + '] is a code'
+msg = f'{first} {last} is a coder'
+print(msg)
